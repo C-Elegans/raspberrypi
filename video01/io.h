a404d44f618edb806aadbc3da6100ba51568b57d
C-Elegans <mtnolan2640@gmail.com>
2014-11-22 16:27:22 -0500

    well, that didn't work

--- video01/io.h
+++ video01/io.h
@@ -1,3 +1,4 @@
 inline void PUT32(unsigned int address,unsigned int data);
 inline void PUT16(unsigned int address,unsigned int data);
 inline void PUT8(unsigned int address,unsigned int data);
+
--- video01/video01.c
+++ video01/video01.c
@@ -130,10 +130,7 @@ int notmain ( void )
     //drawTriangle(x0,y0,x1,y1,x2,y2,color);
     //clrScreen(0b1111100000000000);
 	int j;
-	int volatile data;
-	for(j=1000000;j!=0;j--){
-	data = GET32(0x40040020);
-	}
+	
     hexstring(GetTimeStamp()-timestamp);
     clrScreen(0x0000);
 	}
